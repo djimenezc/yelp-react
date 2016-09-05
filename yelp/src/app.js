b07c2684c539867d6a35674ba47bc28e0fa0a213
David Jimenez <david.jimenez19@gmail.com>
2016-09-05 19:12:23 +0200

    using local package, fixed problem with the version of the history package, using v2

--- yelp/src/app.js
+++ yelp/src/app.js
@@ -9,9 +9,10 @@ import App from 'containers/App/App'
 import {hashHistory} from 'react-router'
 import makeRoutes from './routes'
 
-const routes = makeRoutes()
+const routes = makeRoutes();
 
 const mountNode = document.querySelector('#root');
+
 ReactDOM.render(
   <App history={hashHistory}
         routes={routes} />,
